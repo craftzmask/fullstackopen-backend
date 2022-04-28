b5c983c2a03a655d4f0700a6866b5935efc82592
Khanh Chung <khanh.d.chung@gmail.com>
2022-04-27 17:06:37 -0700

    3.3: Phonebook backend step3

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-let phonebook = [
+app.use(express.json())
+
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -32,7 +34,18 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebook)
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
