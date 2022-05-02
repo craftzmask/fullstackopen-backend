a08ec6591728d0c5595683489a02ebbbd79f566c
Khanh Chung <khanh.d.chung@gmail.com>
2022-05-02 09:57:59 -0700

    3.16: Phonebook database, step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,13 +5,13 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
-app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 
 morgan.token(
   'body', 
-  function (req, res) { return JSON.stringify(req.body) }
+  function (request, response) { return JSON.stringify(req.body) }
 )
 
 app.use(morgan(
@@ -25,10 +25,11 @@ app.get('/info', (request, response) => {
   `)
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person
     .find({})
     .then(persons => response.json(persons))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -42,7 +43,7 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!(body.name && body.number)) {
@@ -56,16 +57,32 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
   
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
