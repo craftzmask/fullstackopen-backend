cafc67b7f237049fb6297494a806360f901d7600
Khanh Chung <khanh.d.chung@gmail.com>
2022-05-12 18:19:59 -0700

    4.18: bloglist expansion, step6

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controller/blogs')
 const usersRouter = require('./controller/users')
+const loginRouter = require('./controller/login')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
